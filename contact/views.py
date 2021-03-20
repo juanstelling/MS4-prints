f303cb18866dc3ba754347c4abde1eb7ddcaba68
juanstelling <juan.stelling@gmail.com>
2021-03-20 19:48:45 +0000

    Added contact form, model and functionality

--- contact/views.py
+++ contact/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
-
+from .forms import ContactForm
 
 
 def contact(request):
     """ A view to return the contact page """
 
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.info(
+                request, 'Your message is send. '
+                + 'We will reply within 48 hours!')
+        return redirect(reverse('contact'))
+    else:
+        form = ContactForm()
 
     template = 'contact/contact.html'
-
-    return render(request, template)
+    context = {
+        'form': form,
+    }
+    return render(request, template, context)
