006f62a92f2127933c10fea9e877f8345e2b9696
juanstelling <juan.stelling@gmail.com>
2021-03-22 15:20:12 +0000

    Added pages templates

--- prnts/settings.py
+++ prnts/settings.py
@@ -16,8 +16,8 @@ import dj_database_url
 if os.path.exists("env.py"):
     import env
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
