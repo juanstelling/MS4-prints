1c7d41b88b51a9d98e4a9f465397ecbab27cb649
juanstelling <juan.stelling@gmail.com>
2021-03-22 20:39:03 +0000

    Added products on homepage

--- home/views.py
+++ home/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
+
+from products.models import Product
 from newsletter.forms import EmailSubscribeForm
 
 
 def index(request):
     """ View to return the index page """
 
+    products = Product.objects.all()[:6]
     form = EmailSubscribeForm()
 
     context = {
+        'products': products,
         'form': form,
     }
 
