d73d41cfb8988eaffa34e14832a762e03c27f327
juanstelling <juan.stelling@gmail.com>
2021-03-25 14:16:21 +0000

    Correct error from pep8 validator

--- checkout/views.py
+++ checkout/views.py
@@ -72,7 +72,7 @@ def checkout(request):
                     )
                     order.delete()
                     return redirect(reverse('view_bag'))
- 
+
             request.session['save_info'] = 'save-info' in request.POST
             return redirect(
                 reverse('checkout_success', args=[order.order_number]))
